2f0034188c1c1aaddc88b59e0f2b7c4d03567e03
ArchieOps <jonathanleonardo123@gmail.com>
2025-06-15 23:23:13 +0700

    add be script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Membuat image menggunakan dockerfile dengan nama image <username-docker>/karsajobs:latest
+docker build -t archieops/karsajobs:latest .
+# Melihat daftar image di dalam host machine
+docker images
+# Mengubah nama dan tag image ke sebelum di push ke ghcr.io dengan nama item-app:v1
+docker tag archieops/karsajobs:latest ghcr.io/archieops/karsajobs:latest
+# Login ke ghcr.io
+echo $GHCRTOKEN | docker login ghcr.io --username ArchieOps --password-stdin
+# Push image ke ghcr.io
+docker push ghcr.io/archieops/karsajobs:latest
